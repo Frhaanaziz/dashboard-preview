a0d5b8d18980b67daff4705b5eb09c37984df7fa
Farhan Aziz <farhan@aththariq.com>
2025-01-12 20:15:18 +0700

    feat: Integrate AccountSection and add visitor/customer stats card to dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,10 @@
+import AccountSection from "@/components/dashboard/AccountSection";
 import ConversionsSection from "@/components/dashboard/ConversionsSection";
 import EarningsSection from "@/components/dashboard/EarningsSection";
 import GrossSalesSection from "@/components/dashboard/GrossSalesSection";
 import StatsSection from "@/components/dashboard/StatsSection";
+import { Card } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
 
 const Page = () => {
   return (
@@ -17,7 +20,22 @@ const Page = () => {
             <ConversionsSection className="col-span-5 mt-10 lg:mt-0" />
           </div>
         </div>
-        {/* <div className="col-span-4"></div> */}
+
+        <div className="col-span-4 mt-10 space-y-10 xl:mt-0">
+          <AccountSection />
+
+          <Card className="flex justify-between p-6">
+            <div className="flex flex-col items-center justify-center">
+              <p className="h3 text-color-2">750K</p>
+              <p className="text-color-4">Website Visitors</p>
+            </div>
+            <Separator orientation="vertical" className="h-18 bg-slate-700" />
+            <div className="flex flex-col items-center justify-center">
+              <p className="h3 text-color-2">7,500</p>
+              <p className="text-color-4">New Customers</p>
+            </div>
+          </Card>
+        </div>
       </div>
     </>
   );
