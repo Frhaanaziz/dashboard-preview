ee985f17878a4507e3aa0b622e32c9577191ec3d
Farhan Aziz <farhan@aththariq.com>
2025-01-12 15:29:48 +0700

    feat: Add RadialStat component for displaying percentage-based statistics

---
+++ src/components/charts/RadialStat.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { Label, PolarRadiusAxis, RadialBar, RadialBarChart } from "recharts";
+
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+import ArrowUpRight from "../icons/arrow-up-right";
+
+interface Props {
+  percentage: number;
+  amount: number;
+}
+
+export default function RadialStat({ percentage, amount }: Props) {
+  const chartData = [{ amount, fill: "var(--color-amount)" }];
+
+  const chartConfig = {
+    amount: {
+      label: "Amount",
+      color: "hsl(var(--chart-2))",
+    },
+  } satisfies ChartConfig;
+
+  return (
+    <ChartContainer config={chartConfig} className="aspect-square min-h-[70px]">
+      <RadialBarChart
+        data={chartData}
+        startAngle={90}
+        endAngle={90 + percentage}
+        innerRadius={33}
+        outerRadius={42}
+      >
+        <ChartTooltip content={<ChartTooltipContent />} />
+        <RadialBar dataKey="amount" background cornerRadius={10} />
+        <PolarRadiusAxis tick={false} tickLine={false} axisLine={false}>
+          <Label
+            content={({ viewBox }) => {
+              if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                return (
+                  <ArrowUpRight
+                    x={viewBox.cx ? viewBox.cx - 10 : undefined}
+                    y={viewBox.cy ? viewBox.cy - 10 : undefined}
+                  />
+                );
+              }
+            }}
+          />
+        </PolarRadiusAxis>
+      </RadialBarChart>
+    </ChartContainer>
+  );
+}
