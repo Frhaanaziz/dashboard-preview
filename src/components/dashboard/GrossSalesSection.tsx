811447d907bff67629e8551ce442261c4d0a0b97
Farhan Aziz <farhan@aththariq.com>
2025-01-12 17:24:22 +0700

    feat: Refactor GrossSalesSection to accept props and use section element

--- src/components/dashboard/GrossSalesSection.tsx
+++ src/components/dashboard/GrossSalesSection.tsx
@@ -1,41 +1,44 @@
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import AreaSale from "../charts/AreaSale";
 import { ChevronDown } from "lucide-react";
+import { ComponentPropsWithoutRef } from "react";
 
-const GrossSalesSection = () => {
+const GrossSalesSection = (props: ComponentPropsWithoutRef<"section">) => {
   return (
-    <Card>
-      <CardHeader>
-        <div className="flex items-end justify-between">
-          <div>
-            <p className="h5">$855.8K</p>
-            <p className="text-color-4">Gross Sales</p>
-          </div>
+    <section {...props}>
+      <Card>
+        <CardHeader>
+          <div className="flex items-end justify-between">
+            <div>
+              <p className="h5">$855.8K</p>
+              <p className="text-color-4">Gross Sales</p>
+            </div>
 
-          <div className="my-auto flex items-center gap-4">
-            <div className="flex items-center gap-2">
-              <div className="h-[13px] w-[13px] rounded-full bg-[#3A57E8]" />
-              <p className="text-color-4">Sales</p>
+            <div className="my-auto flex items-center gap-4">
+              <div className="flex items-center gap-2">
+                <div className="h-[13px] w-[13px] rounded-full bg-[#3A57E8]" />
+                <p className="text-color-4">Sales</p>
+              </div>
+              <div className="flex items-center gap-2">
+                <div className="h-[13px] w-[13px] rounded-full bg-[#85F4FA]" />
+                <p className="text-color-4">Cost</p>
+              </div>
             </div>
-            <div className="flex items-center gap-2">
-              <div className="h-[13px] w-[13px] rounded-full bg-[#85F4FA]" />
-              <p className="text-color-4">Sales</p>
+            <div className="my-auto hidden items-center gap-4 md:flex">
+              <p className="text-color-4">This Week</p>
+              <ChevronDown
+                size={24}
+                strokeWidth={1.4}
+                className="text-zinc-500"
+              />
             </div>
           </div>
-          <div className="my-auto hidden items-center gap-4 md:flex">
-            <p className="text-color-4">This Week</p>
-            <ChevronDown
-              size={24}
-              strokeWidth={1.4}
-              className="text-zinc-500"
-            />
-          </div>
-        </div>
-      </CardHeader>
-      <CardContent>
-        <AreaSale />
-      </CardContent>
-    </Card>
+        </CardHeader>
+        <CardContent>
+          <AreaSale />
+        </CardContent>
+      </Card>
+    </section>
   );
 };
 
