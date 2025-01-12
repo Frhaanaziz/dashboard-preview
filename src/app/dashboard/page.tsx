b1762727d3be20a3b8cc1c0572bf2509ebb542be
Farhan Aziz <farhan@aththariq.com>
2025-01-12 16:21:34 +0700

    feat: Integrate GrossSalesSection into dashboard page layout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,19 @@
+import GrossSalesSection from "@/components/dashboard/GrossSalesSection";
 import StatsSection from "@/components/dashboard/StatsSection";
 
 const Page = () => {
-  return <StatsSection />;
+  return (
+    <>
+      <StatsSection />
+
+      <div className="mt-10 px-8 xl:grid xl:grid-cols-12 xl:gap-10">
+        <div className="col-span-8 space-y-8">
+          <GrossSalesSection />
+        </div>
+        <div className="col-span-4"></div>
+      </div>
+    </>
+  );
 };
 
 export default Page;
