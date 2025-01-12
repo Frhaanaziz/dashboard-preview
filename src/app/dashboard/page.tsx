308304664ba5fc8a814fcdd19b262abc4231bb30
Farhan Aziz <farhan@aththariq.com>
2025-01-12 17:25:00 +0700

    feat: Integrate EarningsSection into dashboard page layout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,4 @@
+import EarningsSection from "@/components/dashboard/EarningsSection";
 import GrossSalesSection from "@/components/dashboard/GrossSalesSection";
 import StatsSection from "@/components/dashboard/StatsSection";
 
@@ -9,6 +10,11 @@ const Page = () => {
       <div className="mt-10 px-8 xl:grid xl:grid-cols-12 xl:gap-10">
         <div className="col-span-8 space-y-8">
           <GrossSalesSection />
+
+          <div className="lg:grid lg:grid-cols-10 lg:gap-10">
+            <EarningsSection className="lg:col-span-5" />
+            <div className="col-span-5 mt-10 lg:mt-0"></div>
+          </div>
         </div>
         <div className="col-span-4"></div>
       </div>
